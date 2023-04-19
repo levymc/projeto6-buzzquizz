6ede2dcfe405e67e6f42c7f99213cf02aed8da4c
rafaelanobre <raafanobre@gmail.com>
2023-04-19 16:42:52 -0300

    perguntas criacao: html e estilo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 axios.defaults.headers.common['Authorization'] = 'seuTokenDeAcessoNoHUB';
+
+function expandirPergunta(){
+    //quando clica na div .nome-pergunta
+    //adiciona a classe expandida e remove a classe escondida
+    //usar o toggle
+    //o primeiro elemento da div deve começar com a classe expandida
+}
