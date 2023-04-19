e58c1f3d2fcbba09a3a83c2232f84ffe4d77b6b2
levymc <levymcruz@gmail.com>
2023-04-19 17:27:02 -0300

    colocando todos os quizzes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 axios.defaults.headers.common['Authorization'] = 'C57BZDyeqoO9fanyGKeKwmBW';
 
+let divPrincipal = document.querySelector(".container-todos");
+let divQuiz = document.querySelector(".quiz")
+
+let renderizaQuizzes = (listaQuizzes) => {
+    // divPrincipal.innerHTML = "";
+    listaQuizzes.forEach(quiz => {
+        console.log(quiz);
+        divPrincipal.innerHTML += `<div class="quiz"><img src="${quiz.image}" alt=""><h4>${quiz.title}</h4></div>`
+        // divQuiz.style.backgroundImage = `url("../img/unnamed.png")`;
+    });
+}
+
+
 let recebeQuizzes = () => {
     axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes").then(response => {
-        console.log(response.data);
-        return response.data
+        renderizaQuizzes(response.data);
     }).catch(error => {
         console.log(error);
         alert("Algum erro ocorreu, tente novamente mais tarde!")
     })
 }
+
 
 recebeQuizzes()
