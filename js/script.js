106823170350550706ee407969a7290e96f8a1a3
levymc <levymcruz@gmail.com>
2023-04-19 17:07:21 -0300

    recebendo os quizzes do servidor

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 axios.defaults.headers.common['Authorization'] = 'C57BZDyeqoO9fanyGKeKwmBW';
+
+let recebeQuizzes = () => {
+    axios.get("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes").then(response => {
+        console.log(response.data);
+        return response.data
+    }).catch(error => {
+        console.log(error);
+        alert("Algum erro ocorreu, tente novamente mais tarde!")
+    })
+}
+
+recebeQuizzes()
