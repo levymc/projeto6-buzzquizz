24492f0491ff360ccce65cd82ecb142e3b8adcde
levymc <levymcruz@gmail.com>
2023-04-18 22:10:30 -0300

    conferencia do URL

---
+++ test.js
@@ -0,0 +1,9 @@
+
+try {
+    new URL("https://st.depositphotos.com/1010338/2099/i/600/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg")
+    console.log("AE")
+    return true;
+} catch (error) {
+    console.log(error)
+    return false;  
+}
