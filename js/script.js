f6bdcc40b92c79550ed8bf0f121676a8016de8d8
levymc <levymcruz@gmail.com>
2023-04-24 16:19:37 -0300

    att

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ let levels = []
 let dadosQuiz ;
 let userIds = [];
 let contador = 0
-let listaIds = [] ;
 
 
 let pontosMaximo = 0;
@@ -23,6 +22,7 @@ let pontosFeitos = 0;
 
 let imgstring="";
 let index;
+let listaIds = [] ;
 
 
 let infoCriarQuiz = {
@@ -38,6 +38,7 @@ let niveisQuiz;
 // Seção de Funções
 
 let renderizarTela1 = () => {
+    
     container.innerHTML = '';
     container.innerHTML = `<div class="container-usuario1 mostrar">
                 <h4>Você não criou nenhum <br />quizz ainda :(</h4>
@@ -719,6 +720,9 @@ function salvarPerguntasQuiz (){
 }
 
 let renderizarTela3_4 = (id) => {
+    listaIds.push(id);
+    localStorage.setItem('ids', JSON.stringify(listaIds));
+    console.log(listaIds);
     container.innerHTML = '';
     container.innerHTML += `
     <div class="tela3_3 flex">
@@ -861,14 +865,9 @@ function btnFinalizarQuizz(){
         }
         
     })
-
-
-    
-    console.log(dadosQuiz);
     levels = [];
     if(contador === 0){
         console.log("valido")
-        
         niveis.forEach((nivel) =>{
             
             const objNivel = {
@@ -887,7 +886,7 @@ function btnFinalizarQuizz(){
         axios.post("https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes", dadosQuiz).then(response => {
             console.log(response.data.id);
             console.log("IDS:::: ", listaIds)
-            localStorage.setItem('ids', JSON.stringify(listaIds));
+            
             renderizarTela3_4(response.data.id);
         }).catch(error => {
             console.log(error);
@@ -924,14 +923,14 @@ let userQuizzes = document.querySelector(".userQuizzes");
 let renderizaQuizzes = (listaQuizzes) => {
     index = listaQuizzes;
     let divPrincipal = document.querySelector(".container-todos");
+    
     if (localStorage.getItem("ids")){
-       listaIds.push(JSON.parse(localStorage.getItem("ids")));
+       userIds.push(JSON.parse(localStorage.getItem("ids")));
     }
     divPrincipal.innerHTML = "";
-    console.log("Aqui", listaIds)
     listaQuizzes.forEach(quiz => {
-        listaIds.forEach(id => {
-            if (quiz.id == id[0][0]){
+        userIds.forEach(id => {
+            if (quiz.id == id[0]){
                 trocarDivUsuario();
                 userQuizzes.innerHTML+=`<div class="quiz" data-test="my-quiz" style="
                 background:linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 65.1%, #000000 100%), url(${quiz.image});
